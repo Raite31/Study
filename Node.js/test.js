e891ab37f27ff9a14f9164c9733ba3b670c65960
Key <60214514+Raite31@users.noreply.github.com>
2022-12-17 18:58:07 +0800

    feat：Node.js连接数据库

--- Node.js/test.js
+++ Node.js/test.js
@@ -1,22 +1,42 @@
 // 引入http模块
-var http = require('http');
+// var http = require('http');
 // 引入URL模块
-var url = require('url');
+// var url = require('url');
+
 // 创建http服务
-var server = http.createServer(function (req, res) {
-  if (req.url == '/index?userId=123') {
-    console.log(req.url);
-    res.write('index');
-    res.end();
-    var urlobj = url.parse(req.url);
-    console.log(urlobj);
-  } else if (req.url == '/login') {
-    console.log(req.url);
-    res.write('login');
-    res.end();
-  }
-});
+// var server = http.createServer(function (req, res) {
+//   if (req.url == '/index?userId=123') {
+//     console.log(req.url);
+//     res.write('index');
+//     res.end();
+//     var urlobj = url.parse(req.url);
+//     console.log(urlobj);
+//   } else if (req.url == '/login') {
+//     console.log(req.url);
+//     res.write('login');
+//     res.end();
+//   }
+// });
+
 // 监听端口号
-server.listen(8888, function () {
-  console.log('start server');
+// server.listen(8888, function () {
+//   console.log('start server');
+// });
+
+// 连接数据库
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'Lee',
+  password: '123456',
+  database: 'day11',
+});
+
+connection.connect(function (err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server.');
 });
