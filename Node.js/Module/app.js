30a4aa946b4e7ebcaf57e6f6d744797a4a3a8f7a
Key <60214514+Raite31@users.noreply.github.com>
2022-12-18 18:17:32 +0800

    feat：练习

--- Node.js/Module/app.js
+++ Node.js/Module/app.js
@@ -6,17 +6,25 @@
 // logger.log('message');
 
 // Path module ========================================================
-const path = require('path');
-var pathObj = path.parse(__filename);
-console.log(pathObj);
+// const path = require('path');
+// var pathObj = path.parse(__filename);
+// console.log(pathObj);
 
 // OS module ========================================================
-const os = require('os');
-const { Module } = require('module');
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
-// Template String
-console.log(`Total Memory： ${totalMemory}`);
-console.log(`Free Memory： ${freeMemory}`);
+// const os = require('os');
+// const { Module } = require('module');
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
+// // Template String
+// console.log(`Total Memory： ${totalMemory}`);
+// console.log(`Free Memory： ${freeMemory}`);
 
 // File Module =========================================================
+const fs = require('fs');
+const files = fs.readdirSync('./');
+console.log(files);
+
+fs.readdir('./', function (err, files) {
+  if (err) console.log('Error', err);
+  else console.log('Result', files);
+});
