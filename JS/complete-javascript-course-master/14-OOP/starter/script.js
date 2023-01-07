30001851cedc2379e83d15b7fc3052c308f09341
Key <60214514+Raite31@users.noreply.github.com>
2023-01-07 18:17:32 +0800

    练习代码

---
+++ JS/complete-javascript-course-master/14-OOP/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+};
+
+console.log(account.latest);
