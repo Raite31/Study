2e5b5dbbfcce11f205d724b00cbd2670bb429a22
Key <60214514+Raite31@users.noreply.github.com>
2023-01-19 17:45:45 +0800

    feat：在express框架使用数据库

---
+++ Node.js/Express/express-locallibrary-tutorial/lib/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const mongoDB = 'mongodb://127.0.0.1/my_database'
+mongoose.connect(mongoDB)
+
+mongoose.Promise = global.Promise
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB 连接错误：'))
