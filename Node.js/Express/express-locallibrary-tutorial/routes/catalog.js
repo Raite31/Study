2c4376a717690a3d9f4987f10477395d8ea63b2a
Key <60214514+Raite31@users.noreply.github.com>
2023-01-30 17:03:59 +0800

    feat：路由补充

--- Node.js/Express/express-locallibrary-tutorial/routes/catalog.js
+++ Node.js/Express/express-locallibrary-tutorial/routes/catalog.js
@@ -41,7 +41,46 @@ router.get('/genres', genre_controller.genre_list);
 
 /// 藏书副本、藏书种类、作者的路由与藏书路由结构基本一致，只是无需获取主页 ///
 
-
+router.get('/genre/create', genre_controller.genre_create_get);
+router.post('/genre/create', genre_controller.genre_create_post);
+router.get('/genre/:id/delete', genre_controller.genre_delete_get);
+router.post('/genre/:id/delete', genre_controller.genre_delete_post);
+router.get('/genre/:id/update', genre_controller.genre_update_get);
+router.post('/genre/:id/update', genre_controller.genre_update_post);
 router.get('/genre/:id', genre_controller.genre_detail);
+
+router.get('/author/create', author_controller.author_create_get);
+router.post('/author/create', author_controller.author_create_post);
+router.get('/author/:id/delete', author_controller.author_delete_get);
+router.post('/author/:id/delete', author_controller.author_delete_post);
+router.get('/author/:id/update', author_controller.author_update_get);
+router.post('/author/:id/update', author_controller.author_update_post);
+router.get('/author/:id', author_controller.author_detail);
+
+router.get(
+  '/bookinstance/create',
+  bookinstance_controller.bookinstance_create_get
+);
+router.post(
+  '/bookinstance/create',
+  bookinstance_controller.bookinstance_create_post
+);
+router.get(
+  '/bookinstance/:id/delete',
+  bookinstance_controller.bookinstance_delete_get
+);
+router.post(
+  '/bookinstance/:id/delete',
+  bookinstance_controller.bookinstance_delete_post
+);
+router.get(
+  '/bookinstance/:id/update',
+  bookinstance_controller.bookinstance_update_get
+);
+router.post(
+  '/bookinstance/:id/update',
+  bookinstance_controller.bookinstance_update_post
+);
+router.get('/bookinstance/:id', bookinstance_controller.bookinstance_detail);
 
 module.exports = router;
