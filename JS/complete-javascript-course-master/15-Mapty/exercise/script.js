2edc1ddf6e56f6d8280aaf15e54df3b2e2a4a884
Key <60214514+Raite31@users.noreply.github.com>
2023-06-08 16:44:30 +0800

    feat：使用leaflet地图

--- JS/complete-javascript-course-master/15-Mapty/exercise/script.js
+++ JS/complete-javascript-course-master/15-Mapty/exercise/script.js
@@ -11,10 +11,13 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+// 确保这个浏览器方法存在
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
+      // 成功时候的回调
       console.log(position);
+      // 获取经纬度
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(
@@ -22,19 +25,27 @@ if (navigator.geolocation)
       );
       console.log(`https://map.baidu.com/@${latitude},${longitude},13z`);
 
-      const map = L.map('map').setView([51.505, -0.09], 13);
+      // leaflet的基本使用
+      // L 是leaflet的入口
+      // 这里的'map'是html里map id的元素，要把它set到map id的标签中
 
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      // 配置用什么地图打开
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([51.5, -0.09])
+      // 配置锚点
+      L.marker(coords)
         .addTo(map)
         .bindPopup('A pretty CSS popup.<br> Easily customizable.')
         .openPopup();
     },
     function () {
+      // 失败时候的回调
       alter('Could not get current position');
     }
   );
