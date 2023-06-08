ada7df667e8f8930ce1048384a8e7f1421a795a0
Key <60214514+Raite31@users.noreply.github.com>
2023-06-08 17:13:34 +0800

    feat: leaflet添加锚点的功能

--- JS/complete-javascript-course-master/15-Mapty/exercise/script.js
+++ JS/complete-javascript-course-master/15-Mapty/exercise/script.js
@@ -43,6 +43,24 @@ if (navigator.geolocation)
         .addTo(map)
         .bindPopup('A pretty CSS popup.<br> Easily customizable.')
         .openPopup();
+
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker({ lat, lng })
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout ')
+          .openPopup();
+      });
     },
     function () {
       // 失败时候的回调
