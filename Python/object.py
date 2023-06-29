ab46710d689d0b04a89e0545320841c104596b47
Key <60214514+Raite31@users.noreply.github.com>
2023-06-29 21:43:14 +0800

    feat：python面向对象练习

--- Python/object.py
+++ Python/object.py
@@ -1,17 +1,26 @@
-class Dog:
-    def __init__(self, name, age):
+class Cat:
+    def __init__(self, name, age, gender):
         self.name = name
         self.age = age
+        self.gender = gender
 
-    def bark(self):
-        print("汪汪！")
+    def introduce(self):
+        print(f"大家好，我的名字叫{self.name}，今年{self.age}岁，我是{self.gender}生")
+
+    def meow(self):
+        print("喵喵~~~~~~~~")
+
+    def run(self):
+        print(f"{self.name}正在奔跑。")
 
     def eat(self, food):
         print(f"{self.name}正在吃{food}。")
 
 
-# 创建一个小狗对象
-my_dog = Dog("旺财", 3)
-# 调用小狗对象的方法
-my_dog.bark()
-my_dog.eat("牛肉")
+# 创建一个小猫对象
+my_cat = Cat("小花", 3, '男')
+# 调用小猫对象的方法
+my_cat.introduce()
+my_cat.meow()
+my_cat.run()
+my_cat.eat("牛肉")
