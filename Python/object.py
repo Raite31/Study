a01fc2a0b557d0a5e1017ca3326b11e5eb97ebb2
Key <60214514+Raite31@users.noreply.github.com>
2023-06-29 22:56:43 +0800

    feat：python面向对象练习

--- Python/object.py
+++ Python/object.py
@@ -1,3 +1,6 @@
+import types
+
+
 class Cat:
     def __init__(self, name, age, gender):
         self.name = name
@@ -5,7 +8,8 @@ class Cat:
         self.gender = gender
 
     def introduce(self):
-        print(f"大家好，我的名字叫{self.name}，今年{self.age}岁，我是{self.gender}生")
+        print(
+            f"大家好，我的名字叫{self.name}，今年{self.age}岁，我是{self.gender}生。")
 
     def meow(self):
         print("喵喵~~~~~~~~")
@@ -17,10 +21,35 @@ class Cat:
         print(f"{self.name}正在吃{food}。")
 
 
-# 创建一个小猫对象
+# 创建一个小猫对象，它叫小花
 my_cat = Cat("小花", 3, '男')
 # 调用小猫对象的方法
 my_cat.introduce()
 my_cat.meow()
 my_cat.run()
 my_cat.eat("牛肉")
+
+my_cat.color = "我的颜色是橘黄色"
+print(my_cat.color)
+
+my_cat.color = "我的颜色是白色"
+print(my_cat.color)
+
+
+def jump(self):
+    print(f"{self.name}我啊，会蹦蹦跳跳的")
+
+
+my_cat.jump = types.MethodType(jump, my_cat)
+my_cat.jump()
+
+
+print('\n')
+
+# 再创建一个小猫对象，它叫小黑
+your_cat = Cat("小黑", 3, '女')
+# 调用小猫对象的方法
+your_cat.introduce()
+your_cat.meow()
+your_cat.run()
+your_cat.eat("鸡肉冻干")
