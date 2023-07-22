1856e09304a178aff0d2ecadd4ac6450cdbd2dac
李嘉胜 <2330165939@qq.com>
2023-07-23 00:09:36 +0800

    feat: 学习python

---
+++ Python/Mosh_class/app.py
@@ -0,0 +1,11 @@
+print("Ljs 你真帅")
+price = 10
+name = 'Mosh'
+is_published = True  # 大小写敏感
+print('price', print)
+print(name)
+
+# 病人
+name = input('What is your name? ', )
+favorite_color = input('What is your favorite color? ')
+print(name + ' likes ' + favorite_color)
