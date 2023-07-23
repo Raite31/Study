9915ed6cc35b456f5cbb32b5393858ecca5e0e8e
李嘉胜 <2330165939@qq.com>
2023-07-23 14:58:09 +0800

    feat: 学习python

--- Python/Mosh_class/app.py
+++ Python/Mosh_class/app.py
@@ -1,3 +1,5 @@
+import math
+
 # print("Ljs 你真帅")
 # price = 10
 # name = 'Mosh'
@@ -5,12 +7,72 @@
 # print('price', print)
 # print(name)
 
-# # 病人
+# # ===========================================病人案例
 # name = input('What is your name? ', )
 # favorite_color = input('What is your favorite color? ')
 # print(name + ' likes ' + favorite_color)
 
 # 计算年龄
-birth_year = input('Birth year? ')
-age = 2023 - int(birth_year)
-print(age)
+# birth_year = input('Birth year? ')
+# age = 2023 - int(birth_year)
+# print(age)
+
+# ===========================================数组
+# course = '''Python's Course for Beginners
+# dadajdia1
+# 的玩家打击'''
+# print(course)
+# print(course[0])
+# print(course[-1])
+# print(course[0:3])  # 取左不取右
+# print(course[3:])  # 取右不取左
+# print(course[:5])  # 取左取右
+# print(course[:])  # 复制一整个字符串
+
+# name = 'Jennifrt'
+# print(name[1:-1])  # 去左去右
+
+# ===========================================字符串拼接
+# first = 'John'
+# last = 'Smith'
+# message = first + ' [' + last + '] is a coder'
+# msg = f'{first} [{last}] is a coder'
+# print(message)
+# print(msg)
+
+# ===========================================字符串方法
+# course = 'Python for Beginners'
+# print(len(course))
+# print(course.upper())
+# print(course.lower())
+# print(course)
+
+# print(course.find('P'))  # 区分大小写
+# print(course.replace('P', 'J'))
+
+# print('Python' in course)
+
+# ===========================================运算符
+# print(10 % 3)
+# print(10 ** 3)
+# x = 10
+# x = x+3
+# x += 3
+
+# print(x)
+
+# ===========================================运算符优先级
+# x = 10 + 3*2
+# print(x)
+# # 幂 乘除 加减
+# x = 10 + 3**2
+# print(x)
+
+# 数学函数
+# x = 2.9
+# print(round(x))
+# print(abs(-2.9))
+
+# print(math.ceil(2.9))
+# print(math.floor(2.9))
+
