19fdeb55c0f20789c119da25f8ab0f93ae393775
李嘉胜 <2330165939@qq.com>
2023-08-07 19:58:23 +0800

    feat: webpack

---
+++ Webpack/webpack-demo/src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash'
+
+function component() {
+	const element = document.createElement('div');
+
+	// lodash（目前通过一个script引入）对于执行这一行是必需的
+    // lodash在当前script中使用import引入
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	return element;
+}
+
+document.body.appendChild(component());
