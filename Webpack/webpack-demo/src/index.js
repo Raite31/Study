318902b21d61780e0a99b4819cc67b9180cb37eb
李嘉胜 <2330165939@qq.com>
2023-08-08 19:59:56 +0800

    feat：webpack

--- Webpack/webpack-demo/src/index.js
+++ Webpack/webpack-demo/src/index.js
@@ -1,12 +1,20 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import './style.css';
+import Icon from './miaowa.jpeg';
 
 function component() {
 	const element = document.createElement('div');
 
 	// lodash（目前通过一个script引入）对于执行这一行是必需的
-    // lodash在当前script中使用import引入
+	// lodash在当前script中使用import引入
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    
+	element.classList.add('hello');
+
+	const myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
+
 	return element;
 }
 
--- Webpack/webpack-demo/webpack.config.js
+++ Webpack/webpack-demo/webpack.config.js
@@ -1,9 +1,29 @@
+const { type } = require('os');
 const path = require('path');
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: 'main.js',
+		// filename: 'main.js',
+		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				// 应保证 loader 的先后顺序
+				// 如果不遵守此约定，webpack 可能会抛出错误
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|wofff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			}
+		],
 	},
 };
