fa81ad3a9da94e05b5453ec884e5893c860347b7
李嘉胜 <2330165939@qq.com>
2023-08-12 16:04:38 +0800

    feat: webpack 管理输出

--- Webpack/webpack-demo/src/index.js
+++ Webpack/webpack-demo/src/index.js
@@ -1,37 +1,19 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './miaowa.jpeg';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml'
-import yaml from './data.yaml'
-import json from './data.json5'
-
-console.log(toml.title);
-console.log(toml.owner.name);
-
-console.log(yaml.title);
-console.log(yaml.owner.name);
-
-console.log(json.title);
-console.log(json.owner.name);
+import printMe from './print';
 
 function component() {
 	const element = document.createElement('div');
+	const btn = document.createElement('button');
 
 	// lodash（目前通过一个script引入）对于执行这一行是必需的
 	// lodash在当前script中使用import引入
 	// 看不懂：https://www.webpackjs.com/guides/getting-started/#creating-a-bundle
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	element.classList.add('hello');
 
-	const myIcon = new Image();
-	myIcon.src = Icon;
+	btn.innerHTML = 'click me and check the console!';
+	btn.onclick = printMe
 
-	element.appendChild(myIcon);
-
-	console.log(Data)
-	console.log(Notes);
+	element.appendChild(btn)
 
 	return element;
 }
--- Webpack/webpack-demo/webpack.config.js
+++ Webpack/webpack-demo/webpack.config.js
@@ -1,71 +1,24 @@
 const { type } = require('os');
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	// entry: './src/index.js',
+
+	entry: {
+		index: './src/index.js',
+		print: './src/print.js',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: '管理输出',
+		}),
+	],
 	output: {
 		// filename: 'main.js',
-		filename: 'bundle.js',
+		// filename: 'bundle.js',
+		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
-	},
-	module: {
-		rules: [
-			// 处理css的loader
-			{
-				test: /\.css$/i,
-				// 应保证 loader 的先后顺序
-				// 如果不遵守此约定，webpack 可能会抛出错误
-				use: ['style-loader', 'css-loader'],
-			},
-			// ===================================== Assets Modules就是webpack内置的处理器
-
-			// 使用Assets Modules 处理图片
-			// 这是内置的Asset Modules
-			// 图像会被处理并添加到output目录
-			// url('./my-image.png')。loader 会识别这是一个本地文件，并将 './my-image.png' 路径，替换为 output 目录中图像的最终路径。而 html-loader 以相同的方式处理 <img src="./my-image.png" />。
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: 'asset/resource',
-			},
-			// 使用 Asset Modules 可以接收并加载任何文件，然后将其输出到构建目录。
-			{
-				test: /\.(woff|wofff2|eot|ttf|otf)$/i,
-				type: 'asset/resource',
-			},
-			// 处理数据的loader
-			{
-				test: /\.(csv|tsv)$/i,
-				use: ['csv-loader'],
-			},
-			{
-				test: /\.xml$/i,
-				use: ['xml-loader'],
-			},
-			// 自定义json模块parser
-			{
-				test: /\.toml$/i,
-				type: 'json',
-				parser: {
-					parse: toml.parse,
-				},
-			},
-			{
-				test: /\.yaml$/i,
-				type: 'json',
-				parser: {
-					parse: yaml.parse,
-				},
-			},
-			{
-				test: /\.json5$/i,
-				type: 'json',
-				parser: {
-					parse: json5.parse,
-				},
-			},
-		],
+		clean: true
 	},
 };
