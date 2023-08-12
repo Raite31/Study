8c9a9baba504efe2b4633bffd16ada2af72eef4b
李嘉胜 <2330165939@qq.com>
2023-08-12 17:08:22 +0800

    feat: webpack 代码分离

--- Webpack/webpack-demo/webpack.config.js
+++ Webpack/webpack-demo/webpack.config.js
@@ -6,9 +6,23 @@ module.exports = {
 	// entry: './src/index.js',
 	mode: 'development',
 	entry: {
-		index: './src/index.js',
-		print: './src/print.js',
+		// index: './src/index.js',
+		// print: './src/print.js',
+		// another: './src/another-module.js',
+
+		// 在多个chunk之间共享模块，防止重复
+		index: {
+			import: './src/index.js',
+			dependOn: 'shared',
+		},
+		another: {
+			import: './src/another-module.js',
+			dependOn: 'shared',
+		},
+		shared: 'lodash',
 	},
+	
+
 	devtool: 'inline-source-map',
 	// 告知 dev server，从什么位置查找文件
 	// 以上配置告知 webpack-dev-server，将 dist 目录下的文件 serve 到 localhost:8080 下
