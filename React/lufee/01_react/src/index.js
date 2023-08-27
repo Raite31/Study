510ced5cc3e465941dc506593cb9b3570acd5a85
李嘉胜 <2330165939@qq.com>
2023-08-27 11:12:02 +0800

    feat: 循环与过滤，注意jsx的写法

--- React/lufee/01_react/src/index.js
+++ React/lufee/01_react/src/index.js
@@ -26,14 +26,41 @@ function getGeeting(user) {
 // ReactDOM.render(getGeeting(user), document.querySelector('#root'));
 
 // React只会更新它需要更新的部分
-function tick() {
-	const element = (
-		<div>
-            {/* h1是不更新的，只有h2发生更新 */}
-			<h1>Hello, world!</h1>
-			<h2>{new Date().toLocaleTimeString()}.</h2>
-		</div>
-	);
-	ReactDOM.render(element, document.querySelector('#root'));
-}
-setInterval(tick, 1000);
+// function tick() {
+// 	const element = (
+// 		<div>
+//             {/* h1是不更新的，只有h2发生更新 */}
+// 			<h1>Hello, world!</h1>
+// 			<h2>{new Date().toLocaleTimeString()}.</h2>
+// 		</div>
+// 	);
+// 	ReactDOM.render(element, document.querySelector('#root'));
+// }
+// setInterval(tick, 1000);
+
+// 循环绑定元素，使用map，不能用forEach，因为forEach没有返回值
+const arr = ['1', '2', '3'];
+const ulEle = (
+	<ul>
+		{arr.map((item, index) => {
+			// 循环绑定的jsx元素，必须要有key属性
+			return <li key={index}>{item}</li>;
+		})}
+	</ul>
+);
+
+const goods = [
+	{ id: 1, price: 100, title: '小米6手机' },
+	{ id: 2, price: 200, title: '小米7手机' },
+	{ id: 3, price: 1100, title: '小米8手机' },
+	{ id: 4, price: 2000, title: '小米9手机' },
+];
+// ReactDOM.render(ulEle, document.querySelector('#root'));
+const filterEle = (
+	<ul>
+		{goods.map((good, index) => {
+			return good.price > 1000 ? <li key={good.id}>{good.title}</li> : null;
+		})}
+	</ul>
+);
+ReactDOM.render(filterEle, document.querySelector('#root'));
