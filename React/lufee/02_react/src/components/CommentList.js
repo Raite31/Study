b5559d81b80ad11270323aa47c9fc0f42f30e95a
李嘉胜 <2330165939@qq.com>
2023-09-16 23:05:12 +0800

    feat：组件渲染优化

--- React/lufee/02_react/src/components/CommentList.js
+++ React/lufee/02_react/src/components/CommentList.js
@@ -1,14 +1,58 @@
-import React, { Component } from 'react';
-function Comment(props){
-    const {id,content,author} = props.comment
-    return(
-        <div>
-            <p>{id}</p>
-            <p>{content}</p>
-            <p>{author}</p>
-        </div>
-    )
-}
+import { render } from '@testing-library/react';
+import React, { Component, PureComponent } from 'react';
+
+// 模拟场景， 监听服务器数据变化，刷新页面组件 但是不一直监听
+// 1. 第一种解决方案
+// class Comment extends Component {
+// 	shouldComponentUpdate(nextProps, nextState) {
+// 		// 性能优化的方法
+// 		if (nextProps.comment.content === this.props.comment.content) {
+// 			return false;
+// 		} else {
+// 			return true;
+// 		}
+// 	}
+// 	render() {
+// 		console.log('render');
+// 		const { id, content, author } = this.props.comment;
+// 		return (
+// 			<div>
+// 				<p>{id}</p>
+// 				<p>{content}</p>
+// 				<p>{author}</p>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// 2. 第二种解决方案： PureComponent 重写了shouldComponentUpdate, 它以浅比较的方式实现的函数类 浅比较比较的是值
+// 浅比较，比较的是值
+// class Comment extends PureComponent {
+// 	render() {
+// 		console.log('render');
+// 		const { id, content, author } = this.props;
+// 		return (
+// 			<div>
+// 				<p>{id}</p>
+// 				<p>{content}</p>
+// 				<p>{author}</p>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// 3. 第三种解决方案 高阶组件
+const Comment = React.memo(({ id, content, author }) => {
+	console.log('render');
+	return (
+		<div>
+			<p>{id}</p>
+			<p>{content}</p>
+			<p>{author}</p>
+		</div>
+	);
+});
+
 // 聪明式组件设计成类 傻瓜式组件仅单纯展示
 export default class CommentList extends Component {
 	constructor(props) {
@@ -37,15 +81,20 @@ export default class CommentList extends Component {
 	}
 	render() {
 		return (
-            <div>
-                {
-                    this.state.comments.map((item,i)=>{
-                        return(
-                            <Comment key={item.id} comment={item}/>
-                        )
-                    })
-                }
-            </div>
-        );
+			<div>
+				{this.state.comments.map((item, i) => {
+					return (
+						// <Comment key={item.id} comment={item} />
+						// <Comment
+						// 	key={item.id}
+						// 	id={item.id}
+						// 	content={item.content}
+						// 	author={item.author}
+						// />
+						<Comment key={item.id} {...item} />
+					);
+				})}
+			</div>
+		);
 	}
 }
