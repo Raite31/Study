100b70bf3d1508ae87821da846f665d36c878fb8
李嘉胜 <2330165939@qq.com>
2023-09-26 00:20:26 +0800

    feat: useState和React中 render的理解

--- React/布鲁克/react-crash-course/src/pages/Home/index.js
+++ React/布鲁克/react-crash-course/src/pages/Home/index.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+
 import Edit from './components/Edit';
 import List from './components/List';
 import './index.css';
 
 const Home = () => {
+	const [a, setA] = useState(100);
+	function plus() {
+		setA(function (prev) {
+			return prev + 200;
+		});
+	}
+
 	return (
-		<div className='app'>
+		<div className="app">
+			{a}
+			<button onClick={plus}>加法</button>
 			<Edit></Edit>
 			<List></List>
 		</div>
