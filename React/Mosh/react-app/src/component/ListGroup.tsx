d89e450a78e96b29c9ee222fe551fbaa4d0b3494
李嘉胜 <2330165939@qq.com>
2023-10-04 01:31:10 +0800

    feat: Handling Event

--- React/Mosh/react-app/src/component/ListGroup.tsx
+++ React/Mosh/react-app/src/component/ListGroup.tsx
@@ -1,11 +1,17 @@
 import { Fragment } from 'react';
+import { MouseEvent } from 'react';
 
 function ListGroup() {
-	// const items = ['New York', 'San Franciso', 'Tokyo', 'London', 'Paris'];
-	const items = []
+	const items = ['New York', 'San Franciso', 'Tokyo', 'London', 'Paris'];
+	// const items = []
 
 	const getMessage = () => {
 		return items.length === 0 ? <p>No item found</p> : null;
+	};
+
+    // ts类型绑定
+	const handleClick = (event: MouseEvent, item: string, index: number) => {
+		console.log(event, item, index);
 	};
 
 	return (
@@ -16,8 +22,11 @@ function ListGroup() {
 			{getMessage()}
 			{items.length === 0 && <p>No item found</p>}
 			<ul className="list-group">
-				{items.map((item) => (
-					<li key={item}>{item}</li>
+				{items.map((item, index) => (
+					<li className="list-group-item" key={item} onClick={handleClick}>
+						{/* onClick中调用的事件，不能有括号，不然就不是点击时才调用 而是一运行就调用了 */}
+						{item}
+					</li>
 				))}
 			</ul>
 		</Fragment>
