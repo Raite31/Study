280ab3b57f36885f5dbbb763488abaf66f43697b
李嘉胜 <2330165939@qq.com>
2023-10-04 01:21:50 +0800

    feat: rending List \ conditional Rendering

--- React/Mosh/react-app/src/component/ListGroup.tsx
+++ React/Mosh/react-app/src/component/ListGroup.tsx
@@ -1,17 +1,24 @@
 import { Fragment } from 'react';
 
 function ListGroup() {
+	// const items = ['New York', 'San Franciso', 'Tokyo', 'London', 'Paris'];
+	const items = []
+
+	const getMessage = () => {
+		return items.length === 0 ? <p>No item found</p> : null;
+	};
+
 	return (
 		// 一个return里不能有多个根标签
 		// 我们可以使用<></>空标签, 也可以使用Fragment
 		<Fragment>
 			<h1>List</h1>
+			{getMessage()}
+			{items.length === 0 && <p>No item found</p>}
 			<ul className="list-group">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-				<li className="list-group-item">A fourth item</li>
-				<li className="list-group-item">And a fifth one</li>
+				{items.map((item) => (
+					<li key={item}>{item}</li>
+				))}
 			</ul>
 		</Fragment>
 	);
