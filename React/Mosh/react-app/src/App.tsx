79507488a8f0486aac79cf8d630a19baa4d5f419
李嘉胜 <2330165939@qq.com>
2023-10-06 00:27:19 +0800

    feat: Exercise: Showing an Alert

--- React/Mosh/react-app/src/App.tsx
+++ React/Mosh/react-app/src/App.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import Alert from './component/Alert';
 import Button from './component/Button';
 
 function App() {
+	const [alertVisibile, setAlertVisibility] = useState(false);
+
 	return (
 		<div>
 			{/* <Alert text="Hello World"></Alert> */}
@@ -9,7 +12,10 @@ function App() {
 				Hello <span>World</span>
 			</Alert> */}
 			{/* <Button color="secondary" onClick={() => console.log('clicked')}>My Button</Button> */}
-			<Button onClick={() => console.log('clicked')}>My Button</Button>
+			{alertVisibile && (
+				<Alert onClose={() => setAlertVisibility(false)}>My alter</Alert>
+			)}
+			<Button onClick={() => setAlertVisibility(true)}>My Button</Button>
 		</div>
 	);
 }
--- React/Mosh/react-app/src/component/Alert.tsx
+++ React/Mosh/react-app/src/component/Alert.tsx
@@ -5,10 +5,21 @@ interface Props {
 	// text: string;
 	// children: string;
 	children: ReactNode;
+	onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-	return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClose }: Props) => {
+	return (
+		<div className="alert alert-primary alert-dismissible">
+			{children}
+			<button
+				type="button"
+				className="btn-close"
+                onClick={onClose}
+				data-bs-dismiss="alert"
+			></button>
+		</div>
+	);
 };
 
 export default Alert;
