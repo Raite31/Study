26391724683cc4910852d7778ce071f75ddb5799
李嘉胜 <2330165939@qq.com>
2023-10-12 09:12:50 +0800

    feat：创建项目

---
+++ React/租房web项目/hkzf-mobile/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return <div className="App">项目根组件</div>;
+}
+
+export default App;
