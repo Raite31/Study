ad13e2e4e12bede3223ed192ce8c072e038392ac
李嘉胜 <2330165939@qq.com>
2023-10-15 23:38:51 +0800

    feat：项目开始

--- React/租房web项目/hkzf-mobile/src/App.jsx
+++ React/租房web项目/hkzf-mobile/src/App.jsx
@@ -1,10 +1,34 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+
+// 页面级别组件
+import Home from './pages/Home';
+import CityList from './pages/CityList';
+
+import { Button } from 'antd-mobile';
 
 function App() {
-	const [count, setCount] = useState(0);
+	// const [count, setCount] = useState(0);
 
-	return <div className="App">项目根组件</div>;
+	return (
+		<BrowserRouter>
+			{/* <div className="App"> */}
+			{/* 项目根组件
+				<Button color="primary" fill="solid">
+					登录
+				</Button> */}
+			<Routes>
+				{/* 配置导航菜单 */}
+				{/* <Link to="/home">首页</Link>
+				<Link to="/citylist">城市选择</Link> */}
+
+				{/* 配置路由 */}
+				<Route path="/home" element={<Home />}></Route>
+				<Route path="/citylist" element={<CityList />}></Route>
+			</Routes>
+			{/* </div> */}
+		</BrowserRouter>
+	);
 }
 
 export default App;
