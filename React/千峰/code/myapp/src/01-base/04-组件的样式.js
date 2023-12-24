34fe75e0fe618a40771ace942141fdd77bdcb148
李嘉胜 <2330165939@qq.com>
2023-12-24 22:53:34 +0800

    feat：事件绑定

--- React/千峰/code/myapp/src/01-base/04-组件的样式.js
+++ React/千峰/code/myapp/src/01-base/04-组件的样式.js
@@ -2,6 +2,14 @@
  * @Author: 李嘉胜 2330165939@qq.com
  * @Date: 2023-12-24 21:52:21
  * @LastEditors: 李嘉胜 2330165939@qq.com
+ * @LastEditTime: 2023-12-24 22:37:20
+ * @FilePath: /Study/React/千峰/code/myapp/src/01-base/04-组件的样式.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
+ * @Author: 李嘉胜 2330165939@qq.com
+ * @Date: 2023-12-24 21:52:21
+ * @LastEditors: 李嘉胜 2330165939@qq.com
  * @LastEditTime: 2023-12-24 22:21:44
  * @FilePath: /Study/React/千峰/code/myapp/01-base/04-组件的样式.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -21,9 +29,10 @@ export default class App extends Component {
 				{10 + 20}-{myname}
 				{10 > 20 ? 'aaa' : 'bbb'}
 				<div style={obj}>1111111</div>
+				{/* React推荐使用行内样式，因为React觉得每个组件都是一个独立的整体 */}
 				<div className="active">333333</div>
 				<div id="myapp">55555555555</div>
-
+				
 				<label for="username">用户名：</label>
 				<input type="text" id="username"></input>
 			</div>
---
+++ React/千峰/code/myapp/src/01-base/05-事件绑定-1.js
@@ -0,0 +1,48 @@
+/*
+ * @Author: 李嘉胜 2330165939@qq.com
+ * @Date: 2023-12-24 22:37:12
+ * @LastEditors: 李嘉胜 2330165939@qq.com
+ * @LastEditTime: 2023-12-24 22:51:43
+ * @FilePath: /Study/React/千峰/code/myapp/src/01-base/05-事件绑定-1.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	a = 100;
+  
+	render() {
+		return (
+			<div>
+				<input></input>
+				<button
+					onClick={() => {
+						console.log('add');
+					}}
+				>
+					add1
+				</button>
+
+				{/* 这两种要留意this指向 */}
+				<button onClick={this.handleClick2}>add2</button>
+				<button onClick={this.handleClick3}>add3</button>
+				<button
+					onClick={() => {
+						this.handleClick4(); // 比较推荐
+					}}
+				>
+					add4
+				</button>
+			</div>
+		);
+	}
+	handleClick2() {
+		console.log('add2');
+	}
+	handleClick3 = () => {
+		console.log('add3');
+	};
+	handleClick4 = () => {
+		console.log('add4');
+	};
+}
