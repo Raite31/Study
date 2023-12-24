cde681c4982ebac3c2311b0f5ea5b9993f7249e4
李嘉胜 <2330165939@qq.com>
2023-12-24 16:35:15 +0800

    feat: 组件的创建--类组件

---
+++ React/千峰/code/myapp/src/01-base/01-class组件.js
@@ -0,0 +1,32 @@
+// class Test {
+// 	constructor() {
+// 		// 成员变量
+// 		this.a = 1;
+// 	}
+// 	// 成员函数
+// 	testa() {
+// 		console.log('testa');
+// 	}
+// }
+
+// // 类的继承
+// class ChildTest extends Test {
+// 	testb() {
+// 		console.log('testb');
+// 	}
+// }
+
+// var obj = new Test();
+// obj.testa();
+// // console.log(obj.testa());
+// console.log(obj.a);
+
+import React from 'react';
+// 组件类，只有继承了React.Component的类才有资格叫组件类
+class App extends React.Component {
+	render() {
+		return <div>hello react Component</div>;
+	}
+}
+
+export default App;
--- React/千峰/code/myapp/src/index.js
+++ React/千峰/code/myapp/src/index.js
@@ -1,9 +1,14 @@
+// 17版本之后 可以不引入React
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './01-base/01-class组件';
 
+// 17版本
+// 不需要引入React，在babel编译的时候会自动引入
+// 使用jsx语法，不需要再用React.createElement语法
 ReactDOM.render(
-	<div>
-		<b>11111111</b>
-	</div>,
+	// jsx 不要加引号
+	// <div>111111</div>
+	<App></App>,
 	document.getElementById('root')
 );
