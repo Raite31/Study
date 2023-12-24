555074d41310e7e261a9f7bde62b3d4a08726ea7
李嘉胜 <2330165939@qq.com>
2023-12-24 22:23:45 +0800

    feat: 组件样式

---
+++ React/千峰/code/myapp/src/01-base/04-组件的样式.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: 李嘉胜 2330165939@qq.com
+ * @Date: 2023-12-24 21:52:21
+ * @LastEditors: 李嘉胜 2330165939@qq.com
+ * @LastEditTime: 2023-12-24 22:21:44
+ * @FilePath: /Study/React/千峰/code/myapp/01-base/04-组件的样式.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React, { Component } from 'react';
+import './css/01-index.css'; // 导入css模块   webpack的支持
+
+export default class App extends Component {
+	render() {
+		var myname = 'Lee';
+		var obj = {
+			background: 'yellow',
+			fontSize: '30px',
+		};
+		return (
+			<div>
+				{10 + 20}-{myname}
+				{10 > 20 ? 'aaa' : 'bbb'}
+				<div style={obj}>1111111</div>
+				<div className="active">333333</div>
+				<div id="myapp">55555555555</div>
+
+				<label for="username">用户名：</label>
+				<input type="text" id="username"></input>
+			</div>
+		);
+	}
+}
