2bbe051af3fe4cc0b1630c22b0f9b8c191444e7a
李嘉胜 <2330165939@qq.com>
2023-12-28 15:49:17 +0800

    feat: 属性验证

--- React/千峰/code/myapp/src/01-base/Navbar/index.js
+++ React/千峰/code/myapp/src/01-base/Navbar/index.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react';
+import kerwinPropTypes from 'prop-types';
+// console.log(kerwinPropTypes);
 
 export default class Navbar extends Component {
 	state = {
 		// 只能自己内部使用，外部无法改变
 	};
 
-	// 属性 是父组件传来的，this.props
+	// 属性校验，写法二
+	static propTypes = {
+		title: kerwinPropTypes.string,
+		leftshow: kerwinPropTypes.bool,
+	};
 
 	render() {
+		// 属性 是父组件传来的，this.props
 		// console.log(this.props);
 		// ES6解构
 		let { title, leftshow } = this.props;
-        
-        // 接收属性做验证？？？
+
 		return (
 			<div>
-                {leftshow && <button>返回</button>}
+				{leftshow && <button>返回</button>}
 				navbar-{title}
 				<button>home</button>
 			</div>
 		);
 	}
 }
+
+// 接收属性做验证，写法一
+// 会在控制台给出报错
+// 类属性
+// Navbar.propTypes = {
+// 	title: kerwinPropTypes.string,
+// 	leftshow: kerwinPropTypes.bool,
+// };
+
+class Test {
+	a = 1; // 对象属性
+	static b = 100; // 类属性
+}
+
+console.log(Test.b);
+
+var obj = new Test();
+console.log(obj.a);
