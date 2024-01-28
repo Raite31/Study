4ab4c01e64792072116a775e1bd2bc2aac9267da
李嘉胜 <2330165939@qq.com>
2024-01-28 14:41:17 +0800

    feat：React-hooks-useMemo

--- React/千峰/code/myapp/src/03-hooks/08-useCallback2.js
+++ React/千峰/code/myapp/src/03-hooks/08-useCallback2.js
@@ -2,11 +2,11 @@
  * @Author: 李嘉胜 2330165939@qq.com
  * @Date: 2024-01-13 10:56:33
  * @LastEditors: 李嘉胜 2330165939@qq.com
- * @LastEditTime: 2024-01-13 17:51:49
+ * @LastEditTime: 2024-01-28 13:41:19
  * @FilePath: /千峰/code/myapp/src/03-hooks/02-todolist.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AEw
  */
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 // 函数式组件没有生命周期这个概念
 export default function App() {
@@ -27,8 +27,18 @@ export default function App() {
 		settext('');
 	}, [text, list]);
 
-	const handelDel = useCallback(
-		(index) => {
+	// const handelDel = useCallback(
+	// 	(index) => {
+	// 		console.log(index);
+	// 		var newList = [...list];
+	// 		newList.splice(index, 1);
+	// 		setList(newList);
+	// 	},
+	// 	[list]
+	// );
+
+	const handelDel = useMemo(
+		() => (index) => {
 			console.log(index);
 			var newList = [...list];
 			newList.splice(index, 1);
