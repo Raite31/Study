7d9324b73a62b73d7eb3d3d057854266957c7f42
李嘉胜 <2330165939@qq.com>
2024-01-29 23:55:02 +0800

    feat: useContext改造成useReducer+useContext

--- React/千峰/code/myapp/src/03-hooks/15-useReducer3.js
+++ React/千峰/code/myapp/src/03-hooks/15-useReducer3.js
@@ -2,43 +2,78 @@
  * @Author: 李嘉胜 2330165939@qq.com
  * @Date: 2023-12-29 14:49:10
  * @LastEditors: 李嘉胜 2330165939@qq.com
- * @LastEditTime: 2024-01-28 22:30:53
+ * @LastEditTime: 2024-01-29 23:53:49
  * @FilePath: /Study/React/千峰/code/myapp/src/02-advanced/06-中间人模式.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import React, { Component, useContext, useEffect, useState } from 'react';
+import React, {
+	Component,
+	useContext,
+	useEffect,
+	useReducer,
+	useState,
+} from 'react';
 import axios from 'axios';
 import './css/index.css';
+
+const intialState = {
+	info: '',
+	filmList: [],
+};
+
+const reducer = (prevState, action) => {
+	var newState = { ...prevState };
+	switch (action.type) {
+		case 'change-filmList':
+			newState.filmList = action.value;
+			console.log(newState);
+			return newState;
+		case 'change-info':
+			newState.info = action.value;
+			return newState;
+		default:
+			return prevState;
+	}
+};
 
 const GlobalContent = React.createContext(); // 创建context对象
 
 export default function App() {
-	const [filmList, setFilmList] = useState([]);
-	const [info, setInfo] = useState('');
+	const [state, dispatch] = useReducer(reducer, intialState);
+
+	// const [filmList, setFilmList] = useState([]);
+	// const [info, setInfo] = useState('');
 
 	useEffect(() => {
 		axios.get(`/test.json`).then((res) => {
 			// console.log(res.data.data.films);
-			setFilmList(res.data.data.films);
+			// setFilmList(res.data.data.films);
+
+			dispatch({
+				type: 'change-filmList',
+				value: res.data.data.films,
+			});
 		});
 	}, []);
 
 	return (
 		<GlobalContent.Provider
 			value={{
-				call: '打电话',
-				sms: '发短信',
-				info: info,
-				changeInfo: (value) => {
-					// this.setState({
-					// 	info: value,
-					// });
-					setInfo(value);
-				},
+				// call: '打电话',
+				// sms: '发短信',
+				// info: info,
+				// changeInfo: (value) => {
+				// 	// this.setState({
+				// 	// 	info: value,
+				// 	// });
+				// 	setInfo(value);
+				// },
+				state,
+				dispatch,
 			}}
 		>
 			<div>
-				{filmList.map((item) => (
+				{state.filmList.map((item) => (
 					<FilmItem key={item.filmId} {...item}></FilmItem>
 				))}
 
@@ -50,15 +85,20 @@ export default function App() {
 
 function FilmItem(props) {
 	let { name, poster, grade, synopsis } = props;
-	const value = useContext(GlobalContent);
+	const { dispatch } = useContext(GlobalContent);
 
-	console.log(value);
+	// console.log(value);
 	return (
 		<div
 			className="fileitem"
 			onClick={() => {
-				console.log(synopsis);
-				value.changeInfo(synopsis);
+				// console.log(synopsis);
+				// value.changeInfo(synopsis);
+
+				dispatch({
+					type: 'change-info',
+					value: synopsis,
+				});
 			}}
 		>
 			<img src={poster} alt={name}></img>
@@ -69,6 +109,6 @@ function FilmItem(props) {
 }
 
 function FilmDetail() {
-	const value = useContext(GlobalContent);
-	return <div className="filmdetail">detial--{value.info}</div>;
+	const { state } = useContext(GlobalContent);
+	return <div className="filmdetail">detail--{state.info}</div>;
 }
