7c0ae0147c4656d8f1f21ac563e0c94cec2b56bd
李嘉胜 <2330165939@qq.com>
2024-02-04 18:01:10 +0800

    feat：React-ReduX原理-简单源码

--- React/千峰/code/myapp/src/05-redux/redux/store.js
+++ React/千峰/code/myapp/src/05-redux/redux/store.js
@@ -22,10 +22,34 @@ const reducer = (
 		default:
 			return prevState;
 	}
-
 	return prevState;
 };
 
 const store = createStore(reducer);
+
+function createLeeStore(reducer) {
+	var list = [];
+	var state = reducer();
+	function subscribe(callback) {
+		list.push(callback);
+	}
+
+	function dispatch(action) {
+		// 覆盖旧的state
+		state = reducer(state, action);
+		for (var i in list) {
+			list[i] && list[i]();
+		}
+	}
+
+	function getState() {
+		return state;
+	}
+	return {
+		subscribe,
+		dispatch,
+		getState,
+	};
+}
 
 export default store;
