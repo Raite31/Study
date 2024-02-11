5183bfc0a16a1539a684e57d4203140374e66b0f
李嘉胜 <2330165939@qq.com>
2024-02-11 11:34:15 +0800

    feat：React-react-redux原理

--- React/千峰/code/myapp/src/06-react-redux/views/NotFound.js
+++ React/千峰/code/myapp/src/06-react-redux/views/NotFound.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
-export default function NotFound() {
-  return (
-    <div>
-      not found
-    </div>
-  )
+function NotFound(props) {
+	useEffect(() => {
+		console.log(props);
+	}, [props]);
+	return <div>not found</div>;
 }
+
+function leeconnect(cb, obj) {
+	var value = cb();
+	// 第一个return是返回函数，第二个return返回加工后的组件
+	return (MyComponent) => {
+		// 函数式组件
+		return (props) => {
+			console.log(props);
+			return (
+				<div style={{ color: 'red' }}>
+					<MyComponent {...value} {...props} {...obj} />
+				</div>
+			);
+		};
+	};
+}
+export default leeconnect(
+	() => {
+		return {
+			a: 1,
+			b: 2,
+		};
+	},
+	{
+		aa() {},
+		bb() {},
+	}
+)(NotFound);
