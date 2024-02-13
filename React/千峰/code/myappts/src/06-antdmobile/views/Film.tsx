ace2bbc12df797c3674bafc380b17199f2638483
李嘉胜 <2330165939@qq.com>
2024-02-14 00:57:47 +0800

    feat: React-TS-组件库

---
+++ React/千峰/code/myappts/src/06-antdmobile/views/Film.tsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { RouteComponentProps } from 'react-router-dom';
+import { Button, Swiper, SwiperRef } from 'antd-mobile';
+
+interface IItem {
+	filmId: number;
+	name: string;
+}
+// interface IProps {
+// 	history: any;
+// }
+
+export default class Film extends Component<RouteComponentProps, any> {
+	state = {
+		list: [],
+	};
+	ref = React.createRef<SwiperRef>();
+	componentDidMount = () => {
+		axios({
+			url: 'https://m.maizuo.com/gateway?cityId=440100&pageNum=1&pageSize=10&type=2&k=7238895',
+			headers: {
+				'X-Client-Info':
+					'{"a":"3000","ch":"1002","v":"5.2.1","e":"17037263502752202158374913"}',
+				'X-Host': 'mall.film-ticket.film.list',
+			},
+		}).then((res) => {
+			console.log(res.data.data.films);
+			// setlist(res.data.data.films);
+
+			this.setState({ list: res.data.data.films });
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<Swiper loop autoplay ref={this.ref}>
+					{/* <Swiper.Item>111</Swiper.Item>
+					<Swiper.Item>222</Swiper.Item>
+					<Swiper.Item>333</Swiper.Item> */}
+
+					{this.state.list.map((item: any) => {
+						return (
+							<Swiper.Item key={item.filmId}>
+								<img src={item.poster} alt="" style={{ width: '50%' }} />
+							</Swiper.Item>
+						);
+					})}
+				</Swiper>
+				<Button
+					color="danger"
+					onClick={() => {
+						this.ref.current?.swipePrev();
+					}}
+				>
+					上一个
+				</Button>
+				<Button
+					color="primary"
+					onClick={() => {
+						(this.ref.current as SwiperRef).swipeNext();
+					}}
+				>
+					下一个
+				</Button>
+				<ul>
+					{this.state.list.map((item: IItem) => {
+						return (
+							<li
+								key={item.filmId}
+								onClick={() => {
+									// console.log(this.props.history);
+									// this.props.history.push();
+									this.props.history.push(`/detail/${item.filmId}`);
+								}}
+							>
+								{item.name}
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
+}
