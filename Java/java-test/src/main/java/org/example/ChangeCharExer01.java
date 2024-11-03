23436023b5affd9b1741e4df02f0c3f27c2d940d
李嘉胜 <2330165939@qq.com>
2024-11-03 21:27:46 +0800

    feat：Java学习

--- Java/java-test/src/main/java/org/example/ChangeCharExer01.java
+++ Java/java-test/src/main/java/org/example/ChangeCharExer01.java
@@ -11,6 +11,7 @@ public class ChangeCharExer01 {
 //        文档注释
 //        注释内容可被JDK提供的工具javadoc所解析，生成一套以网页文件形式体现的该程序的说明文档，一般写在类
 //        基本格式：/** */
+//        可通过  javadoc -d 文件夹名 -xx -yy Demo3.java  命令生成html文档
         /**
          * @author: 李嘉胜
          * @version: 1.0
--- Java/java-test/src/main/java/org/example/Main.java
+++ Java/java-test/src/main/java/org/example/Main.java
@@ -2,14 +2,48 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("你好");
+//        int n1 = 10;
+//        float d1 = n1 + 1.1; // 错 n1+1.1 => double
+//        double d2 = n1 + 1.1;
+//        float d3 = n1 + 1.1f;
+
+//        byte b1 = 10;
+//        int n2 = 1;
+//        float b2 = n2;
+//        byte b3 = n2; // 错 如果是变量赋值，判断类型
+//        char c1 = b1; // 错 byte不能自动转换成char
+
+//        byte b2 = 1;
+//        byte b3 = 2;
+//        short s1 = 1;
+////        short s2 = b2 + s1; // 错 b2+s1 => int
+//        int s2 = b2 + s1;
+////        byte b4 = b2 + b3; // 错 b2+b3 => int
+//
+//        boolean pass = true;
+////        int num100 = pass; // 错 boolean不参与类型自动转换
+//
+//        byte b4 = 1;
+//        short s3 = 100;
+//        int num200 = 1;
+//        float num300 = 1.1F;
+//        double num500 = b4 + s3 + num200 + num300; // float -> double
+//
+//        int i = (int) 1.9;
+//        System.out.println(i);
+//
+//        int j = 100;
+//        byte b1 = (byte) j;
+//        System.out.println(b1);
+
+////        int x = (int)10*3.5 + 6*1.5; // 编译错误 double -> int
+//        int x = (int) (10 * 3.5 + 6 * 1.5); // (int)44.0 -> 44
+//        System.out.println(x);
+//
+//        char c1 = 100;
+//        int m = 100;
+////        char c2 = m; // 错误
+//        char c3 = (char) m;
+//        System.out.println(c3); // 100对应的字符，d字符
     }
-}
-
-class Dog {
-
-}
-
-class Tiger {
-
 }
