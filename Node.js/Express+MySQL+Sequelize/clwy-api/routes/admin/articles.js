c044b9c575bb21cd787c17f3377f8da34bc271fd
Lee31-ak <lee.li@yttec.net>
2025-06-05 22:50:27 +0800

    feat：查询文章详情接口

--- Node.js/Express+MySQL+Sequelize/clwy-api/routes/admin/articles.js
+++ Node.js/Express+MySQL+Sequelize/clwy-api/routes/admin/articles.js
@@ -1,28 +1,58 @@
 const express = require("express");
 const router = express.Router();
-const { Article } = require("../../models");
+const {Article} = require("../../models");
 
 /**
  * 查询文章列表
  */
 router.get("/", async function (req, res) {
-  try {
-    const condition = {
-      order: [["id", "DESC"]],
-    };
-    const articles = await Article.findAll(condition);
-    res.json({
-      status: true,
-      message: "查询文章列表成功",
-      data: articles,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: false,
-      message: "查询文章列表失败",
-      errors: [error.message],
-    });
-  }
+    try {
+        const condition = {
+            order: [["id", "DESC"]],
+        };
+        const articles = await Article.findAll(condition);
+        res.json({
+            status: true,
+            message: "查询文章列表成功",
+            data: articles,
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: "查询文章列表失败",
+            errors: [error.message],
+        });
+    }
 });
+
+/**
+ * 查询文章详情
+ * GET admin/articles/:id
+ */
+router.get("/:id", async function (req, res) {
+    try {
+        const {id} = req.params
+        const article = await Article.findByPk(id)
+        if (article) {
+            res.json({
+                status: true,
+                message: "查询文章详情成功",
+                data: article,
+            });
+        } else {
+            res.status(404).json({
+                status: false,
+                message: "文章未找到",
+            });
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: "查询文章详情失败",
+            errors: [error.message],
+        });
+    }
+})
 
 module.exports = router;
